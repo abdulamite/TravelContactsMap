a691621dafa24499eb1c8f8793a282ce0282549e
Abdul Shaikh <Abdul Shaikh>
2017-02-17 20:08:08 -0800

    added seperate file to host city data and infowindow code

--- script/main.js
+++ script/main.js
@@ -1,23 +1,36 @@
 // code adapted from netbrains from stack overflow answered Oct 28 '13 at 17:07
 function initMap() {
   var map = new google.maps.Map($('#map')[0],{
-      zoom: 5,
+      zoom: 4,
       center: new google.maps.LatLng(39.828291, -97.676636)
   });
 
-  var cities = ['Arcata, CA', 'Denver, Co', 'Detroit, MI','Montebello, CA','Germany','Denver, CO'];
   var markers = [];
-
-  for (var x = 0; x < cities.length; x++) {
+  var location =[];
+  var infowindow = new google.maps.InfoWindow();
+  for (var x = 0,count = 0; x < cities.length; x++,count++) {
       $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+cities[x]+'&sensor=false', null, function (data) {
           var p = data.results[0].geometry.location
           var latlng = new google.maps.LatLng(p.lat, p.lng);
+          location.push(latlng);
           var marker= new google.maps.Marker({
                       position: latlng,
-                      map: map
+                      map: map,
+                      animation: google.maps.Animation.DROP,
+                      id:cities[x]
                       });
           marker.setMap(map);
-          markers.addMarker(marker);
+          var id = marker.id;
+          markers.push(id);
+
+          // add info windows to each of the markers
+          google.maps.event.addListener(marker, 'click', (function (marker, x) {
+          return function (){
+              infowindow.setContent('Student Contacts: ' + id);
+              infowindow.open(map, marker);
+          }
+        })(marker, x));
       });
   }
+  console.log(markers);
 }
