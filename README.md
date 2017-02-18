df1f3a08d56c87ce535a945f287a7b79474adbd2
Abdul Shaikh <Abdul Shaikh>
2017-02-18 15:52:27 -0800

    markers now display citynames on mouseover

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # TravelContactsMap
 Project to Help Humboldt State University Outreach Program.
 The Purpose of this project is to track see where prospective students are coming from
-
--- script/main.js
+++ script/main.js
@@ -1,36 +1,40 @@
-// code adapted from netbrains from stack overflow answered Oct 28 '13 at 17:07
-function initMap() {
-  var map = new google.maps.Map($('#map')[0],{
-      zoom: 4,
-      center: new google.maps.LatLng(39.828291, -97.676636)
-  });
 
-  var markers = [];
-  var location =[];
-  var infowindow = new google.maps.InfoWindow();
-  for (var x = 0,count = 0; x < cities.length; x++,count++) {
-      $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+cities[x]+'&sensor=false', null, function (data) {
+    var lats = [];
+    var longs = [];
+    var city_names = [];
+
+    for (var x = 0; x < cities.length; x++) {
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+cities[x]+'&sensor=false', null, function (data) {
           var p = data.results[0].geometry.location
-          var latlng = new google.maps.LatLng(p.lat, p.lng);
-          location.push(latlng);
-          var marker= new google.maps.Marker({
-                      position: latlng,
-                      map: map,
-                      animation: google.maps.Animation.DROP,
-                      id:cities[x]
-                      });
-          marker.setMap(map);
-          var id = marker.id;
-          markers.push(id);
-
-          // add info windows to each of the markers
-          google.maps.event.addListener(marker, 'click', (function (marker, x) {
-          return function (){
-              infowindow.setContent('Student Contacts: ' + id);
-              infowindow.open(map, marker);
-          }
-        })(marker, x));
+          lats.push(p.lat);
+          longs.push(p.lng);
       });
-  }
-  console.log(markers);
-}
+    }
+
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: new google.maps.LatLng(39.828291, -97.676636)
+      });
+
+      var infowindow = new google.maps.InfoWindow();
+
+      for (i = 0; i < lats.length; i++) {
+          var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(lats[i], longs[i]),
+              map: map,
+              animation: google.maps.Animation.DROP
+          });
+
+          google.maps.event.addListener(marker, 'mouseover', (function (marker, i) {
+              return function () {
+                  infowindow.setContent(cities[i]);
+                  infowindow.open(map, marker);
+              }
+          })(marker, i));
+
+        google.maps.event.addListener(marker, 'mouseout', function(){
+          infowindow.close();
+        });
+      }
+    }
